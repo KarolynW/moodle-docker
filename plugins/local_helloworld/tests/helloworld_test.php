1bcbdecae6f18e4012aafe057aef23c8806d5035
Karolyn Webb <119711647+KarolynW@users.noreply.github.com>
2025-06-09 11:13:59 +0100

    Updated class names and method names in helloworld_test.php

--- plugins/local_helloworld/tests/helloworld_test.php
+++ plugins/local_helloworld/tests/helloworld_test.php
@@ -1,10 +1,10 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
+   <?php
+   defined('MOODLE_INTERNAL') || die();
 
-use PHPUnit\Framework\TestCase;
 
-class local_helloworld_testcase extends TestCase {
-    public function test_sanity_check() {
-        $this->assertTrue(true);
-    }
-}
+   class helloworld_test extends advanced_testcase {
+       public function test_sanity_check(): void {
+           $this->assertTrue(true);
+       }
+   }
+
