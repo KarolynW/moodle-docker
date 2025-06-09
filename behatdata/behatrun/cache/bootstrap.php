d6d5a41f9cd1d7bf7c4940f9b6d031a256a52820
Karolyn Webb <119711647+KarolynW@users.noreply.github.com>
2025-06-09 15:22:39 +0100

    Installed Behat - data local in repo for testing

---
+++ behatdata/behatrun/cache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = '4hBtsyi6hQeLNtPYn0J3KRFQNdzw0mZ4localhost';
+$CFG->bootstraphash = '524b3627a57e957b0c885f92949279af';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
